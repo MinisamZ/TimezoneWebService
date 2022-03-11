39914cb16e7cb8420f4438740c6eab70856195b0
Alexandr <minisam2468@gmail.com>
2022-03-12 04:17:15 +0600

    Initial commit

---
+++ src/main/java/kz/iitu/WebServiceTest.java
@@ -0,0 +1,66 @@
+package kz.iitu;
+
+import kz.iitu.model.City;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@WebService
+public class WebServiceTest {
+
+    private TimeZoneDao timeZoneDao = new TimeZoneDao();
+
+    @WebMethod
+    public List<City> getListCityByCode(String countryCodeLocal) {
+        // требуется вырезать т.к подвергает программу логическим ошибкам
+        return timeZoneDao.getListCityByCode(countryCodeLocal);
+    }
+
+    @WebMethod
+    public City getCityByCityName(String s) throws Exception {
+        System.out.println(1);
+        List<City> ret = timeZoneDao.getListCity();
+        for (City city : ret) {
+            if (city.cityName.equals(s)) {
+                return city;
+            }
+        }
+        throw new Exception("Not found city");
+    }
+
+    @WebMethod
+    public String getString(String s) throws Exception {
+        if (s.equals("1")) {
+            return s;
+        } else
+            throw new Exception("not 1");
+    }
+
+    @WebMethod
+    public List<City> getListCityByZoneName(String s) {
+        List<City> ret = timeZoneDao.getListCity();
+        List<City> result = new ArrayList<>();
+        for (City city : ret) {
+            if (city.zoneName.equals(s)) {
+                result.add(city);
+            }
+        }
+        return result;
+    }
+
+    @WebMethod
+    public List<City> getListCityByZoneNameAndByCode(String zone, String code) {
+        List<City> ret = timeZoneDao.getListCity();
+        List<City> result = new ArrayList<>();
+        for (City city : ret) {
+            if (city.zoneName.equals(zone) && city.countryCode.equals(code.toUpperCase(Locale.ROOT))) {
+                result.add(city);
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/kz/iitu/model/City.java
@@ -0,0 +1,31 @@
+package kz.iitu.model;
+
+public class City {
+    public String countryCode;
+    public String zoneName;
+    public String cityName;
+    public String gmtOffset;
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "countryCode='" + countryCode + '\'' +
+                ", zoneName='" + zoneName + '\'' +
+                ", city='" + cityName + '\'' +
+                ", gmtOffset='" + gmtOffset + '\'' +
+                '}';
+    }
+
+    public String toString2() {
+        if (Double.parseDouble(gmtOffset) > 0)
+            return "City " + cityName + ":" +
+                    "\n zone name: " + zoneName +
+                    "\n country code: " + countryCode +
+                    "\n UTC: + " + gmtOffset;
+        else
+            return "City " + cityName + ":" +
+                    "\n zone name: " + zoneName +
+                    "\n country code: " + countryCode +
+                    "\n UTC: " + gmtOffset;
+    }
+}
